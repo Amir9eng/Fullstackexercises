e68b251cedd715cdafb85e152600bd17f37926bb
Amir9eng <amirmukhtarme@gmail.com>
2021-01-08 17:34:12 +0100

    testing recursion

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return <div>this app is used by pressing the buttons</div>;
+  }
+
+  return <div>button press history: {allClicks.join(" ")}</div>;
+};
+
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
   const handleLeftClick = () => {
     setAll(allClicks.concat("L"));
@@ -15,13 +24,23 @@ const App = () => {
     setRight(right + 1);
   };
 
+  const hello = () => {
+    const handler = () => console.log('i"m here');
+    return handler;
+  };
+
   return (
     <div>
       {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(" ")}</p>
+      <History allClicks={allClicks} />
+
+      <div>
+        {value}
+        <button onClick={hello()}>button</button>
+      </div>
     </div>
   );
 };
