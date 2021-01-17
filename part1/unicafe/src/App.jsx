2c73f69af252839bf154c517cec9fbe6fff8a2ab
Amir9eng <amirmukhtarme@gmail.com>
2021-01-17 17:13:39 +0100

    step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,28 @@
 import { useState } from "react";
 import "./App.css";
-const History = ({ allClicks }) => {
-  if (allClicks.length === 0) {
-    return <div>this app is used by pressing the buttons</div>;
-  }
-
-  return <div>button press history: {allClicks.join(" ")}</div>;
-};
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const average = Math.round(((good - bad) / (good + bad)) * 100) / 100;
+  const positive = Math.round((good / (good + bad)) * 100);
+
   return (
     <div>
-      {left}
-      <button onClick={setGood}>left</button>
-      <button onClick={setBad}>right</button>
-      <History allClicks={setNeutral} />
+      <h2>give Feedback</h2>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}></button>
+      <button onClick={() => setBad(bad + 1)}></button>
+
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average{average}</p>
+      <p>positive {positive}</p>
     </div>
   );
 };
