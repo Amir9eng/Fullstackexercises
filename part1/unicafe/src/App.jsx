e75ca42db5d5c0e5d06cda0d3b15d72141f7f20b
Amir9eng <amirmukhtarme@gmail.com>
2021-01-18 10:17:26 +0100

    step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,28 +1,37 @@
 import { useState } from "react";
 import "./App.css";
 
+const Statistics = ({ good, neutral, bad }) => {
+  const allSum = good + bad + neutral;
+  const sumGoodAndBad = good + bad;
+  const average = Math.round(((good - bad) / sumGoodAndBad) * 100) / 100;
+  const positive = Math.round((good / sumGoodAndBad) * 100);
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {allSum}</p>
+      <p>average {average ? average : 0}</p>
+      <p>positive {positive ? positive : 0} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const average = Math.round(((good - bad) / (good + bad)) * 100) / 100;
-  const positive = Math.round((good / (good + bad)) * 100);
-
   return (
     <div>
       <h2>give Feedback</h2>
       <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}></button>
-      <button onClick={() => setBad(bad + 1)}></button>
-
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average{average}</p>
-      <p>positive {positive} %</p>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
