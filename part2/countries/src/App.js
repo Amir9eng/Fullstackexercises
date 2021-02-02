59cfb3886cf85f9510d43698cb57e75a48644d01
Amir9eng <amirmukhtarme@gmail.com>
2021-02-02 10:30:48 +0100

    Data for countries: step 1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,6 +2,46 @@ import "./App.css";
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
 
+const OneCountry = ({ country }) => {
+  const { name, population, languages, flag, capital } = country[0];
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Capital - {capital}</p>
+      <p>Population - {population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {languages.map((language, languageIndex) => (
+          <li key={`language-number${languageIndex}`}>{language.name}</li>
+        ))}
+      </ul>
+      <img src={flag} alt={`${name}-flag`} style={{ width: "30rem" }} />
+    </div>
+  );
+};
+
+const DisplayingCountries = ({ filtered, countries }) => {
+  const countriesToShow = filtered.trim()
+    ? countries.filter((country) =>
+        country.name.toLowerCase().includes(filtered.toLowerCase())
+      )
+    : [];
+  const countriesLength = countriesToShow.length;
+
+  return (
+    <div>
+      {countriesLength === 1 ? (
+        <OneCountry country={countriesToShow} />
+      ) : countriesLength < 10 ? (
+        countriesToShow.map((country, countryIndex) => (
+          <p key={`country-Index${countryIndex}`}>{country.name}</p>
+        ))
+      ) : (
+        <p>Too many matches, specify another filter </p>
+      )}
+    </div>
+  );
+};
 function App() {
   const [countries, setCountries] = useState([]);
   const [filtered, setFiltered] = useState("");
@@ -11,12 +51,6 @@ function App() {
       .then(({ data }) => setCountries(data))
       .catch((error) => console.log(error));
   }, []);
-
-  const countriesToShow = filtered.trim()
-    ? countries.filter((country) =>
-        country.name.toLowerCase().includes(filtered.toLowerCase())
-      )
-    : [];
 
   return (
     <div className="App">
@@ -30,11 +64,7 @@ function App() {
           />
         </div>
       </form>
-      <div>
-        {countriesToShow.map((country, countryIndex) => (
-          <li key={`country-Index${countryIndex}`}>{country.name}</li>
-        ))}
-      </div>
+      <DisplayingCountries filtered={filtered} countries={countries} />
     </div>
   );
 }
