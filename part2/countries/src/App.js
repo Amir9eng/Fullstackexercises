268cf3fab136af19e4b2c8bbc06382a7c5f0919a
Amir9eng <amirmukhtarme@gmail.com>
2021-02-03 14:39:35 +0100

    Data for countries, step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,8 +2,21 @@ import "./App.css";
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
 
+const apiKey = process.env.REACT_APP_KEY;
+
 const SingleCountry = ({ country }) => {
   const { name, population, languages, flag, capital } = country[0];
+  const [weather, setWeather] = useState("");
+  useEffect(() => {
+    Axios.get(
+      `http://api.weatherstack.com/current?access_key=${apiKey}&query=${capital}`
+    )
+      .then((res) => {
+        console.log(res.data);
+        setWeather(res.data);
+      })
+      .catch((err) => console.log({ err }));
+  }, [capital]);
   return (
     <div>
       <h2>{name}</h2>
@@ -12,16 +25,31 @@ const SingleCountry = ({ country }) => {
       <h2>Languages</h2>
       <ul>
         {languages.map((language, languageIndex) => (
-          <li key={`language-number${languageIndex}`}>{language.name}</li>
+          <li key={`language${languageIndex}`}>{language.name}</li>
         ))}
       </ul>
       <img src={flag} alt={`${name}-flag`} style={{ width: "30rem" }} />
+      {weather && (
+        <div>
+          <h4>Weather in {capital} </h4>
+          <p>Temperature: {weather.current.temperature} celcius</p>
+          <img src={weather.current.weather_icons} alt="weather-icon" />
+          <p>
+            Wind: {weather.current.wind_speed} mph direction{" "}
+            {weather.current.wind_dir}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
 
-const DisplayingCountries = ({ filtered, countries }) => {
-  const [showCountries, setShowCountries] = useState([]);
+const DisplayingCountries = ({
+  filtered,
+  countries,
+  showCountries,
+  setShowCountries,
+}) => {
   const countriesToShow = filtered.trim()
     ? countries.filter((country) =>
         country.name.toLowerCase().includes(filtered.toLowerCase())
@@ -54,6 +82,7 @@ const DisplayingCountries = ({ filtered, countries }) => {
   );
 };
 function App() {
+  const [showCountries, setShowCountries] = useState([]);
   const [countries, setCountries] = useState([]);
   const [filtered, setFiltered] = useState("");
 
@@ -63,6 +92,11 @@ function App() {
       .catch((error) => console.log(error));
   }, []);
 
+  const handleInputChange = (e) => {
+    if (showCountries.length) setShowCountries([]);
+    setFiltered(e.target.value);
+  };
+
   return (
     <div className="App">
       <form>
@@ -71,11 +105,16 @@ function App() {
           <input
             type="text"
             value={filtered}
-            onChange={(e) => setFiltered(e.target.value)}
+            onChange={(e) => handleInputChange(e)}
           />
         </div>
       </form>
-      <DisplayingCountries filtered={filtered} countries={countries} />
+      <DisplayingCountries
+        filtered={filtered}
+        countries={countries}
+        showCountries={showCountries}
+        setShowCountries={setShowCountries}
+      />
     </div>
   );
 }
